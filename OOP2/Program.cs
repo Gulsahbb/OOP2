adbadeeff4fb3077cd63ff255da5293437967cba
Gülşah Başkurt <Gulsah.baskurt@ogr.dpu.edu.tr>
2021-04-09 02:34:30 +0300

    Add project files.

---
+++ OOP2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri gercekMusteri = new GercekMusteri();
+            gercekMusteri.MusteriNo = "1xxx";
+            gercekMusteri.Id = 1;
+            gercekMusteri.Adi = "Gxxxxx";
+            gercekMusteri.Soyadi = "Bxxxxx";
+            gercekMusteri.TcNo = "3xxxxxxxxxxx";
+
+            TuzelMusteri tuzelMusteri = new TuzelMusteri();
+            tuzelMusteri.MusteriNo = "2xxxxx";
+            tuzelMusteri.Id = 2;
+            tuzelMusteri.SirketAdi = "Rxxxxx";
+            tuzelMusteri.VergiNo = "Çxxxxxx";
+
+            Musteri yeniMusteri1 = new GercekMusteri();
+            Musteri yeniMusteri2 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(gercekMusteri);
+            musteriManager.Ekle(tuzelMusteri);
+            musteriManager.Ekle(yeniMusteri1);
+            musteriManager.Ekle(yeniMusteri2);
+
+        }
+    }
+}
